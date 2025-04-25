2ff96fdf083867eb90e54028f7b5939c15c2f7a7
shivam2575 <sk2575@gmail.com>
2025-04-25 20:39:18 +0530

    added search suggestion feature using debounce

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { toogleMenu } from "../utils/appSlice";
 import { Link } from "react-router-dom";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchList, setSearchList] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
   const dispath = useDispatch();
   const toogleMenuHandler = () => {
     dispath(toogleMenu());
   };
+  const getSearchSuggestions = async () => {
+    const response = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await response.json();
+    setSearchList(json[1]);
+    console.log(searchList);
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
   return (
     <div className="grid grid-flow-col p-4 m-2 shadow-lg">
       <div className="flex col-span-1 items-center">
@@ -25,18 +41,36 @@ const Navbar = () => {
           />
         </Link>
       </div>
-      <div className="col-span-10 flex justify-center">
-        <input
-          className="border border-gray-400 p-2 w-1/2 rounded-l-full"
-          type="text"
-        />
-        <button className="border border-gray-400 px-4 py-2 bg-gray-200 rounded-r-full cursor-pointer">
-          <img
-            className="h-6 bg-transparent"
-            src="https://cdn-icons-png.flaticon.com/512/5680/5680244.png"
-            alt="search button"
+      <div className="col-span-10 flex flex-col justify-center items-center">
+        <div className="w-full flex justify-center items-center">
+          <input
+            className="border border-gray-400 p-2 w-1/2 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
-        </button>
+          <button className="border border-gray-400 px-4 py-2 bg-gray-200 rounded-r-full cursor-pointer">
+            <img
+              className="h-6 bg-transparent"
+              src="https://cdn-icons-png.flaticon.com/512/5680/5680244.png"
+              alt="search button"
+            />
+          </button>
+        </div>
+
+        {showSuggestions && searchList.length > 0 && (
+          <div className="w-full relative flex justify-center ">
+            <ul className="absolute w-1/2 bg-white border border-gray-400 rounded-xl shadow-lg mr-14 p-2">
+              {searchList.map((s) => (
+                <li key={s} className="py-2 shadow-sm hover:bg-gray-200">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1 flex items-center">
         <img
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,7 +11,8 @@ export const FILTER_LIST = [
   "Cricket",
   "Live",
 ];
-
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const YOUTUBE_VIDEO_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=" +
   import.meta.env.VITE_YOUTUBE_API_KEY;
